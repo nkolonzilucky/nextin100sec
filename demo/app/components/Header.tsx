a42798c5ef4b4f514192fb3ffd791569027d315c
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2024-11-06 18:34:07 +0000

    experimenting with shadcn-ui

--- demo/app/components/Header.tsx
+++ demo/app/components/Header.tsx
@@ -1,9 +1,13 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div className='bg-zinc-50 h-56 flex items-center justify-center w-auto'>
-    <h1 className='text-5xl sm:text-9xl'>Header</h1>  
+    <div className='bg-zinc-50 h-56 flex flex-col items-center justify-center w-auto'>
+    <h1 className='text-5xl sm:text-9xl'>Header</h1> 
+    <div>
+    <Button>Press Me</Button> 
+    </div>
     </div>
   )
 }
