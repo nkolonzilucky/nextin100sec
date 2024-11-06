113c136adacd6ad3f2a05bff576d2d3e566ddd93
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2024-11-06 17:57:47 +0000

    Added Header and footer components

---
+++ demo/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-white text-center'>
+      <h1>FOOTER</h1>
+    </div>
+  )
+}
+
+export default Footer
---
+++ demo/app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-zinc-50 h-56 flex items-center justify-center w-auto'>
+    <h1 className='text-5xl sm:text-9xl'>Header</h1>  
+    </div>
+  )
+}
+
+export default Header
